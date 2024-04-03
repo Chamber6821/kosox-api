1a53bf6ad6dd7eee46a186bc26b7a2c7a1468372
Chamber6821 <vania.osinin@yandex.ru>
2024-04-03 23:33:38 +0300

    feat(ProductRepository): add findByCategoryId

--- src/main/java/com/kosox/api/v1/ProductRepository.java
+++ src/main/java/com/kosox/api/v1/ProductRepository.java
@@ -1,10 +1,12 @@
 package com.kosox.api.v1;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
 import com.kosox.api.entities.Product;
 
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  public Page<Product> findByCategoryId(Long category, Pageable pageable);
 }
