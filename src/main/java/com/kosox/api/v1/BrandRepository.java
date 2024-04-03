1e1291bb653acfa4a205d88cce428d671d27072a
Chamber6821 <vania.osinin@yandex.ru>
2024-04-03 14:24:14 +0300

    fix: add CrossOrigin annotation

--- src/main/java/com/kosox/api/v1/BrandRepository.java
+++ src/main/java/com/kosox/api/v1/BrandRepository.java
@@ -1,7 +1,10 @@
 package com.kosox.api.v1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import com.kosox.api.entities.Brand;
 
+@CrossOrigin
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 }
--- src/main/java/com/kosox/api/v1/ProductRepository.java
+++ src/main/java/com/kosox/api/v1/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.kosox.api.v1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.kosox.api.entities.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
