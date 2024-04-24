11514c91f81bf4c461095d0fe1d51d45b324a31a
Chamber6821 <vania.osinin@yandex.ru>
2024-04-24 23:16:07 +0300

    feat: add categories/findAllByBrand

--- src/main/java/com/kosox/api/v1/CategoryRepository.java
+++ src/main/java/com/kosox/api/v1/CategoryRepository.java
@@ -1,10 +1,21 @@
 package com.kosox.api.v1;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.kosox.api.entities.Category;
 
 @CrossOrigin
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+  @Query("""
+      SELECT c FROM Category c
+      JOIN c.products pc
+      JOIN pc.brand pcb
+      WHERE pcb.id = :brandId
+      GROUP BY c
+      """)
+  public Set<Category> findAllByBrand(Long brandId);
 }
