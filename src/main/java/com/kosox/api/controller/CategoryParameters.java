492822ece2ed0519ea1e7c74ef33380341605498
Chamber6821 <vania.osinin@yandex.ru>
2024-05-17 15:00:29 +0300

    fix(CategoryParameters): add CrossOrigin

--- src/main/java/com/kosox/api/controller/CategoryParameters.java
+++ src/main/java/com/kosox/api/controller/CategoryParameters.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin
 public class CategoryParameters {
 
   private final ProductParameterRepository parameters;
