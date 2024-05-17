8a2b903d932c5c27fa8a0681861dd32afb94f2b4
Chamber6821 <vania.osinin@yandex.ru>
2024-05-17 13:38:41 +0300

    feat: add category parameters

---
+++ src/main/java/com/kosox/api/controllers/CategoryParameters.java
@@ -0,0 +1,29 @@
+package com.kosox.api.controllers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kosox.api.v1.ProductParameterRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class CategoryParameters {
+
+  private final ProductParameterRepository parameters;
+
+  @GetMapping("/categories/{id}/parameters")
+  public Object parameters(@PathVariable Long id) {
+    return parameters.forCategory(id).stream()
+        .collect(Collectors.toMap(
+            x -> x.name(),
+            x -> List.of(x.value()),
+            (a, b) -> Stream.concat(a.stream(), b.stream()).toList()));
+  }
+}
