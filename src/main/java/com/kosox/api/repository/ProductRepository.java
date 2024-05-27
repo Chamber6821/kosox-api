b4a14361d343176cab3d223dd4050d327a6921c8
Chamber6821 <vania.osinin@yandex.ru>
2024-05-27 18:04:53 +0300

    feat(ProductRepository): add search

--- src/main/java/com/kosox/api/repository/ProductRepository.java
+++ src/main/java/com/kosox/api/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.kosox.api.entity.Product;
@@ -11,4 +12,20 @@ import com.kosox.api.entity.Product;
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
   public Page<Product> findByCategoryId(Long category, Pageable pageable);
+
+  @Query("""
+      SELECT p FROM Product p
+      JOIN p.brand b
+      JOIN p.category c
+      JOIN p.parameters pps
+      WHERE
+        lower(p.name) LIKE concat('%', lower(:fragment), '%')
+        OR lower(b.name) LIKE concat('%', lower(:fragment), '%')
+        OR lower(c.name) LIKE concat('%', lower(:fragment), '%')
+        OR lower(pps.name) LIKE concat('%', lower(:fragment), '%')
+        OR lower(pps.value) LIKE concat('%', lower(:fragment), '%')
+      GROUP BY p
+      ORDER BY p.name
+      """)
+  Page<Product> search(String fragment, Pageable pageable);
 }
