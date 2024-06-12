704f34293e0a9c8c64b747d3d9e07ad7345e7c15
Chamber6821 <vania.osinin@yandex.ru>
2024-06-12 17:11:00 +0300

    feat(CategoryRepository): add method findAllByManufacturer

--- src/main/java/com/kosox/api/repository/CategoryRepository.java
+++ src/main/java/com/kosox/api/repository/CategoryRepository.java
@@ -1,5 +1,8 @@
 package com.kosox.api.repository;
 
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -7,4 +10,13 @@ import com.kosox.api.entity.Category;
 
 @CrossOrigin
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+  @Query("""
+      SELECT c FROM Category c
+      JOIN c.subcategories s
+      JOIN s.products ps
+      JOIN ps.manufacturer psm
+      WHERE psm.id = :manufacturerId
+      GROUP BY c
+      """)
+  public Set<Category> findAllByManufacturer(Long manufacturerId);
 }
